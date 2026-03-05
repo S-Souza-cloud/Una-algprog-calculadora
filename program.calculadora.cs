09a722c03dbab3acf3a74a26cf49bf6630270342
S-Souza-cloud <silascalebe5@gmail.com>
2026-03-05 19:43:41 -0300

    Add files via upload

---
+++ program.calculadora.cs
@@ -0,0 +1,59 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Oi,Silas");
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("=== CALCULADORA ===");
+
+        Console.Write("Digite o primeiro número: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Digite o segundo número: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Escolha a operação:");
+        Console.WriteLine("1 - Soma");
+        Console.WriteLine("2 - Subtração");
+        Console.WriteLine("3 - Multiplicação");
+        Console.WriteLine("4 - Divisão");
+
+        Console.Write("Opção: ");
+        int opcao = Convert.ToInt32(Console.ReadLine());
+
+        double resultado = 0;
+
+        switch (opcao)
+        {
+            case 1:
+                resultado = num1 + num2;
+                break;
+
+            case 2:
+                resultado = num1 - num2;
+                break;
+
+            case 3:
+                resultado = num1 * num2;
+                break;
+
+            case 4:
+                if (num2 != 0)
+                    resultado = num1 / num2;
+                else
+                {
+                    Console.WriteLine("Erro: divisão por zero!");
+                    return;
+                }
+                break;
+
+            default:
+                Console.WriteLine("Opção inválida!");
+                return;
+        }
+
+        Console.WriteLine("Resultado: " + resultado);
+    }
+}
